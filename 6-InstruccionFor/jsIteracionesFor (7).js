f4f1d9c55c59e1f5280ee27dd655f464a7264592
alumno <alumno>
2020-02-27 12:10:07 -0300

    alumno

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,7 +1,22 @@
 function mostrar()
 {
 
+var numero;
+var contadorDivisores = 0;
 
+numero = parseInt(prompt("Ingrese un numero:"));
 
+while (isNaN(numero)) {
+    numero = parseInt(prompt("Eso no es un numero.Ingrese un numero"));
+
+    for( var i=1; i <= numero; i++){
+
+        if (numero % i == 0)
+        console.log(i);
+        contadorDivisores++;
+    }
+}
+
+console.log("Cantidad divisores encontrados:" + contadorDivisores);
 
 }//FIN DE LA FUNCIÓN
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,14 @@
 function mostrar()
 {
-  
+  var nombre;
+  var localidad;
+
+  nombre = document.getElementById("elNombre").value;
+
+  localidad = document.getElementsByTagName("laLocalidad").value;
+
+  alert("Usted se llama" + nombre);
+
+  alert("Vive en la localidad" + localiad);
+
 }
